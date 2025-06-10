e42fcecec7dfa1aa8e58cdc835380bf58b813696
Kairnex <codexkairnex@gmail.com>
2025-06-10 07:57:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 21189715  # Your Bot API ID
 API_HASH = "988a9111105fd2f0c5e21c2c2449edfd"
 BOT_TOKEN = "7717292730:AAENCNXzUqNtbdoqb7xQXNLLDnm0HXEzoN4"
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 ADMINS = [7416620797]  # Your Telegram user ID(s)
 LOG_CHANNEL_ID = -1002550719110
