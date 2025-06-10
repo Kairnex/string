cc733d1a574a593bb9947883f6a93e899fb864f7
Kairnex <codexkairnex@gmail.com>
2025-06-10 07:44:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = 123456  # Your Bot API ID
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+MONGO_URI = "mongodb://localhost:27017"
+ADMINS = [123456789]  # Your Telegram user ID(s)
+LOG_CHANNEL_ID = -1001234567890 
