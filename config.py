600b2b10dd39e81d12e7e92ad695f4e84f5bd3c5
Kairnex <codexkairnex@gmail.com>
2025-06-10 09:10:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ API_ID = 21189715  # Your Bot API ID
 API_HASH = "988a9111105fd2f0c5e21c2c2449edfd"
 BOT_TOKEN = "7717292730:AAENCNXzUqNtbdoqb7xQXNLLDnm0HXEzoN4"
 MONGO_URI = "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-ADMINS = [7416620797]  # Your Telegram user ID(s)
+ADMINS = [7416620797]
+ADMIN_ID = [7416620797]# Your Telegram user ID(s)
 LOG_CHANNEL_ID = -1002550719110
 REQUIRED_CHANNEL = "acxbots"  # no @
 REQUIRED_GROUP = "friend_chat_international"
